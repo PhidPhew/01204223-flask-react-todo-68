33bb5f5be01c6e4ec513835d36320d23c57e734d
Chawankorn Khamtue <phidphew@PhidPhew-2.local>
2026-02-20 13:55:59 +0700

    add it block

--- frontend/src/__tests__/TodoItem.test.jsx
+++ frontend/src/__tests__/TodoItem.test.jsx
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react'
 import { expect } from 'vitest'
 import TodoItem from '../TodoItem.jsx'
 
-const baseTodo = {             // ** TodoItem พื้นฐานสำหรับทดสอบ
+const baseTodo = {             
     id: 1,
     title: 'Sample Todo',
     done: false,
@@ -11,10 +11,28 @@ const baseTodo = {             // ** TodoItem พื้นฐานสำหร�
 
 describe('TodoItem', () => {
   it('renders with no comments correctly', () => {
-    // เดี๋ยวจะเพิ่มโค้ดตรงนี้
     render(
         <TodoItem todo={baseTodo} />
       );
       expect(screen.getByText('Sample Todo')).toBeInTheDocument();
+  });
+
+  it('renders with comments correctly', () => {
+    const todoWithComment = {
+      ...baseTodo,
+      comments: [
+        {id: 1, message: 'First comment'},
+        {id: 2, message: 'Another comment'},
+      ]
+    };
+    
+    render(
+      <TodoItem todo={todoWithComment} />
+    );
+    
+    expect(screen.getByText('Sample Todo')).toBeInTheDocument();
+    
+    expect(screen.getByText('First comment')).toBeInTheDocument();
+    expect(screen.getByText('Another comment')).toBeInTheDocument();
   });
 });
